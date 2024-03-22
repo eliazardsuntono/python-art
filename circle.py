650a54d8d14b8730864f58c74cfee89e75cb6965
eliazardsuntono <eliazardsunton@gmail.com>
2024-03-22 02:48:57 -0400

    minor edits to circle file

--- circle.py
+++ circle.py
@@ -8,11 +8,10 @@ def y_direction(theta):
     return 200*math.cos(theta)
 
 speed(50000000)
-bgcolor("grey")
+bgcolor("black")
 
 for i in range(6000) :
     goto(x_direction(i), y_direction(i))
-    for j in range(1):
-        color("green")
+    color("red")
     goto(0,0)
 done()
